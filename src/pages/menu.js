8580831b548ac212609fae170de9f1c6151329b5
Seyit Enes <seyit7enes@gmail.com>
2023-01-20 08:50:26 +0100

    creating meals

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,7 +1,46 @@
 import "/src/styles/menu.css";
+import noodleImage from "../assets/images/menu-item-image-0.jpg";
 
 export default function menu() {
+  const main = document.querySelector("main");
+  main.innerHTML = "";
+
   const menuTitle = document.createElement("h2");
   menuTitle.textContent = "Our Way, Our Menu";
-  document.body.appendChild(menuTitle);
+  main.appendChild(menuTitle);
+
+  const secondTitle = document.createElement("p");
+  secondTitle.setAttribute("class", "menu-second-title");
+  secondTitle.textContent = "Small menu, endless flavours";
+  main.appendChild(secondTitle);
+
+  const wrapperTitle = document.createElement("h3");
+  wrapperTitle.setAttribute("class", "wrapper-title");
+  wrapperTitle.textContent = "Øur Menu";
+  main.appendChild(wrapperTitle);
+
+  const wrapper = document.createElement("div");
+  wrapper.setAttribute("class", "menu-wrapper");
+  main.appendChild(wrapper);
+  //-----------------------------------------------------
+  function createMenuItem(id, name, image) {
+    const menuItems = document.createElement("div");
+    menuItems.setAttribute("class", "menu-items");
+    menuItems.setAttribute("id", `item-${id}`);
+    wrapper.appendChild(menuItems);
+
+    const itemTitle = document.createElement("h5");
+    itemTitle.setAttribute("class", "item-title");
+    itemTitle.textContent = name;
+    menuItems.appendChild(itemTitle);
+
+    const itemImage = new Image();
+    itemImage.src = image;
+    itemImage.setAttribute("class", "item-image");
+    menuItems.appendChild(itemImage);
+  }
+  createMenuItem(1, "Pizza", noodleImage);
+  createMenuItem(2, "Burger");
+  createMenuItem(3, "Nudle");
+  createMenuItem(4, "Döner");
 }
